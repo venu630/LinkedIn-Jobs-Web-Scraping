550c0fbafa8f7d0b8bda09a8ed6a6d87c609801e
Venu <sirisanagandlavenu@gmail.com>
2024-05-15 23:03:42 -0500

    final commits

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -1,11 +1,3 @@
-# Scrapy settings for linkedin project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'linkedin'
 
